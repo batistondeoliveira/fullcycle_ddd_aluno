6049dcb3f390dcf28b3d5dacb340039d958e17e4
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-07-20 13:21:01 -0300

    feat: verificando jest com Sequelize

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/model/product.model";
 
 describe("Product repository test", () => {
   
@@ -12,8 +13,11 @@ describe("Product repository test", () => {
       sync: { force: true },
     })
 
-    afterEach(async () => {
-      await sequileze.close();
-    });
+    sequileze.addModels([ProductModel]);
+    await sequileze.sync();
+  });
+
+  afterEach(async () => {
+    await sequileze.close();
   });
 });
