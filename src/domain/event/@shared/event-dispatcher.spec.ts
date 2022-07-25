361d5c0132a7b7b1075874c60836331806cacd63
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-07-25 20:46:45 -0300

    test: disparando eventos

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+import ProductCreatedEvent from "../product/product-created.event";
 import EventDispatcher from "./event-dispatcher";
 
 describe("Domain event tests", () => {
@@ -40,5 +41,26 @@ describe("Domain event tests", () => {
     eventDispatcher.unregisterAll();
 
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle"); //indica para o jest ficar espiando se o metodo handle 
+                                                                //foi executado
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product 1",
+      descriptoin: "Product 1 description",
+      price: 10.0
+    });
+
+    //Quando o notify for executado o SendEmailWhenProductIsCreatedHandler.handle() deve ser chamado
+    eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
